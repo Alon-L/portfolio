7001ad3247ff966e1418405a9f0be014f6c11786
DayColor <2005alonlivne@gmail.com>
2020-02-29 17:26:42 +0200

    dev > disabled lazy loading background for full optimization testing

--- src/components/pages/landing/specialties/SpecialtyBox.tsx
+++ src/components/pages/landing/specialties/SpecialtyBox.tsx
@@ -17,24 +17,23 @@ class SpecialtyBox extends Component<Props> {
   render() {
     const { specialty: { title, path: specialtyPath, dimensions: { rows, columns }, background, placeholder }, webpSupported } = this.props;
 
-    const boxStyle: React.CSSProperties = {
-      gridRow: `span ${rows}`,
-      gridColumn: `span ${columns}`,
-    };
-
-    console.log(webpSupported);
     const backgroundPath = path.join('../assets/specialties/home-backgrounds', `${background}.${webpSupported ? 'webp' : 'jpg'}`);
     const placeholderPath = path.join('../assets/specialties/home-backgrounds/placeholders', `${placeholder}.${webpSupported ? 'webp' : 'jpg'}`);
 
+    const boxStyle: React.CSSProperties = {
+      gridRow: `span ${rows}`,
+      gridColumn: `span ${columns}`,
+      backgroundImage: `url(${backgroundPath})`,
+    };
+
     return (
-      <LazyLoadingBackground placeholder={placeholderPath} src={backgroundPath} className="specialty-box"
-        style={boxStyle}>
+      <div className="specialty-box" style={boxStyle}>
         <Link to={specialtyPath} className="color-white d-block h-100">
           <div className="d-flex h-100 justify-content-center align-items-center">
             <h5 className="specialty-title font-weight-bold text-uppercase text-center">{title}</h5>
           </div>
         </Link>
-      </LazyLoadingBackground>
+      </div>
     );
   }
 }
