244cd767acacc3b3efd2d092201fcf3b58c79bdb
DayColor <2005alonlivne@gmail.com>
2020-03-04 22:23:28 +0200

    dev > removed unused imports

--- src/components/pages/projects/ProjectGoBack.tsx
+++ src/components/pages/projects/ProjectGoBack.tsx
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft } from '@fortawesome/pro-solid-svg-icons';
